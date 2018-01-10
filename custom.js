4d8e6f3fd983c29e037af047530a03472129fc53
shoval <shoval.tirman@gmail.com>
2018-01-10 15:12:20 +0200

    Added first version of navbar files
    In addition, custom.css and custom.js were added as examples for installation of the navbar.

---
+++ custom.js
@@ -0,0 +1,10 @@
+/* 
+  The following code should go inside of 'custom.js'.
+  The file should be in: ~/.jupyter/custom/custom.js. 
+  If it doesn't exist, create it.
+*/
+
+/***********
+ * Nav bar *
+ ***********/
+$.getScript("/custom/navbar.js");
---
+++ navbar.js
@@ -0,0 +1,69 @@
+
+navbar = {
+  /************
+   * Settings *
+   ************/
+  list_tag: 'ol',
+  nav_id: 'navbar',
+  
+  
+  /*************
+   * Functions *
+   *************/
+  get_html: function() {
+    var open_tag = '<' + navbar.list_tag + '>';
+    var close_tag = '</' + navbar.list_tag + '>';
+    var all_txt = '';
+    var prev_level = 0;
+    var hs = $('.text_cell_render').find('h1,h2,h3,h4,h5');
+    for (i=0; i<hs.length; i++) {
+      x = hs[i]
+      level = parseInt(x.tagName.substr(1));
+      txt = '<li><a href=#' + x.id + '>' + x.innerText.replace(/¶$/, '') + '</a></li>';
+      diff = level - prev_level;
+      for (j=0; j<diff; j++) {
+        txt = open_tag + txt;
+      }
+      for (j=diff; j<0; j++) {
+        txt = close_tag + txt;
+      }
+      prev_level = level
+      all_txt += txt
+    }
+    all_txt += close_tag
+    return all_txt;
+  },
+
+  rebuild: function() {
+    var nav_obj = $('#'+navbar.nav_id);  
+    nav_obj.html(navbar.get_html());
+    console.log('Nav rebuilt');
+  },
+
+  first_build: function() {
+    console.log('Nav first build');
+    navbar.rebuild();
+    
+    requirejs(['base/js/events'], function (events) {
+      events.on('rendered.MarkdownCell delete.Cell', navbar.rebuild);
+    });
+  },
+  
+  init: function() {
+    console.log('Nav init');
+    // Wrap the main content to allow the resizable nav-bar
+    $('#site').wrap('<div id="main_wrapper"><div id="split_wrapper"><div id="site_wrapper"></div></div></div>');
+    $('<div id="nav_wrapper"><div id="'+navbar.nav_id+'"><small>Loading...</small></div></div>').insertBefore('#site_wrapper');
+    
+    $( window ).resize(function() {
+      $('#nav_wrapper').height($(window).height() - $('#header').height());
+    });
+
+    
+    requirejs(['base/js/events'], function (events) {
+      events.one('kernel_idle.Kernel', navbar.first_build);
+    });
+  },
+}
+
+navbar.init();
